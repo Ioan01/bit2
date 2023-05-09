d3f0a888a428665873b5a4faed09fd6b47fb9ec0
ioan <ioan.macavei2001@gmail.com>
2023-05-09 19:45:39 +0300

    redis

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
 import hashlib
+import os
 
 import motor.motor_asyncio
 import requests
+import redis
+
 import uvicorn
 from fastapi import FastAPI, File, HTTPException
 from typing_extensions import Annotated
 
 from models import eventModel, verdictModel, eventResponseModel
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://root:example@mongo:27017")
+redis_client = redis.Redis(host='redis', port=6379, db=0)
+redis_client.set('a', 'b', )
+
+b = redis_client.get('a')
+print(b)
+
+connectionString = os.getenv("MONGO_URL") or "mongodb://root:example@localhost:27017"
+
+
+client = motor.motor_asyncio.AsyncIOMotorClient(connectionString)
 db = client["data"]
 verdicts = db["verdicts"]
 
 blackbox_path = 'https://beta.nimbus.bitdefender.net/liga-ac-labs-cloud/blackbox-scanner/'
+
+
+async def findCachedOrQueryRiskLevel(hash: str) -> int:
+
+    riskLevel = redis_client.get(hash)
+
+    if riskLevel is None:
+        verdict = await verdicts.find_one({"hash": hash})
+        if verdict is not None:
+            redis_client.set(hash, verdict["risk_level"], 30)
+            return verdict["risk_level"]
+        return -1
+
+    return int(riskLevel)
 
 
 @app.post("/events")
@@ -22,13 +49,11 @@ async def events(event: eventModel) -> eventResponseModel:
     fileVerdict = verdictModel(hash=event.file.file_hash, risk_level=-1)
     processVerdict = verdictModel(hash=event.file.file_hash, risk_level=-1)
 
-    fileVerdictLookup = await verdicts.find_one({"hash": event.file.file_hash})
-    processVerdictLookup = await verdicts.find_one({"hash": event.last_access.hash})
+    fileRiskLevel = await findCachedOrQueryRiskLevel(event.file.file_hash)
+    processRiskLevel = await findCachedOrQueryRiskLevel(event.last_access.hash)
 
-    if fileVerdictLookup is not None:
-        fileVerdict.risk_level = fileVerdictLookup["risk_level"]
-    if processVerdictLookup is not None:
-        processVerdict.risk_level = processVerdictLookup["risk_level"]
+    fileVerdict.risk_level = fileRiskLevel
+    processVerdict.risk_level = processRiskLevel
 
     return eventResponseModel(file=fileVerdict, process=processVerdict)
 
@@ -60,5 +85,6 @@ async def scanFile(file: Annotated[bytes, File()]):
 
 
 if __name__ == '__main__':
+    Instrumentator().instrument(app).expose(app)
     uvicorn.run(app, port=8000, host='0.0.0.0')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
